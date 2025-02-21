4fffdcc273c7a1dd07924e4d95ad1aa530fb7586
MattNeptune24 <matthewsuchter41@icloud.com>
2025-02-20 19:21:35 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -161,3 +161,21 @@ function calculate8(){
     document.getElementById("Display").textContent = "Equation : " + "Circumference of a circle with the radius of " + number1 + " = x, ";
     document.getElementById("Answer").textContent = " x = " + ((2*3.14159)*number1);
 }
+
+function calculate9(){
+    var number1 = parseFloat(document.getElementById("Number1").value);
+    var number2 = parseFloat(document.getElementById("Number2").value);
+    var type = parseFloat(document.getElementById("Type").value);
+    if(type==1){
+        document.getElementById("Display").textContent = "Equation : " + number1 + "% = x, ";
+        document.getElementById("Answer").textContent = "x = " + number1/100 + ", or " + number1 + " / 100";
+    }
+    if(type==2){
+        document.getElementById("Display").textContent = "Equation : " + number1 + " = x, ";
+        document.getElementById("Answer").textContent = "x = " + number1*100 + "%" + ", or " + number1*100 + " / 100";
+    }
+    if(type==3){
+        document.getElementById("Display").textContent = "Equation : " + number1 + " / " + number2 + " = x,";
+        document.getElementById("Answer").textContent = "x = " + (number1/number2)*100 + "%" + ", or " + number1/number2;
+    }
+}
